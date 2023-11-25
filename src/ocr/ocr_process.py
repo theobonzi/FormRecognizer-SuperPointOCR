8e2828936345102a4d7cdf5fd58fe8ba5abb9f17
daniel_rosa <daniel.rosa@epita.fr>
2023-11-25 17:14:16 +0100

    classifier

--- src/ocr/ocr_process.py
+++ src/ocr/ocr_process.py
@@ -63,6 +63,70 @@ def draw_boxes_on_image(image, json_data, nb_ocr):
     df = pd.DataFrame([data_dict])
     return image, strings, df
 
+from PIL import Image
+import os
+
+def filter_labels(data):
+    with open('checkbox_labels.json', 'r') as file:
+        data = json.load(file)
+    valid_labels = set(data["2042"])
+    for item in data:
+        annotations = item.get("annotations", [])
+        for annotation in annotations:
+            results = annotation.get("result", [])
+            # Filter out labels not in valid_labels
+            annotation["result"] = [r for r in results if "labels" in r and any(lbl in valid_labels for lbl in r["value"].get("labels", []))]
+    return data
+
+def cut_boxes_on_image(image, json_data):
+    if isinstance(image, np.ndarray):
+        image = Image.fromarray(image)
+    elif isinstance(image, str):
+        image = Image.open(image)
+
+    strings = []
+    data_dict = {}
+    cropped_images = []
+
+    # Filter the json_data to only keep checkboxes
+    json_data = filter_labels(json_data)
+
+    # Ensure the folder for saving cropped images exists
+    save_directory = "cropped_images"
+    if not os.path.exists(save_directory):
+        os.makedirs(save_directory)
+
+    for index, item in enumerate(json_data):
+        for annotation in item['annotations']:
+            for result in annotation['result']:
+                if result['type'] == 'labels':
+                    value = result['value']
+                    labels = value['labels']
+                    
+                    x = value['x'] * image.width / 100
+                    y = value['y'] * image.height / 100
+                    width = value['width'] * image.width / 100
+                    height = value['height'] * image.height / 100
+                    
+                    print('x:', x)
+                    print('y:', y)
+                    print('width:', width)
+                    print('height:', height)
+
+                    padding = 5
+                    # Cropping the image according to the rectangle
+                    cropped = image.crop((x - padding, y - padding, x + width + padding, y + height + padding))
+                    cropped_images.append(cropped)
+
+                    # Save each cropped image
+                    save_path = os.path.join(save_directory, f"{image}_{index}.jpg")
+                    cropped.save(save_path)
+
+
+    df = pd.DataFrame([data_dict])
+    return cropped_images, strings, df
+
+
 
 
 def draw_boxes(img_path, good_forms, nb_ocr=0):
@@ -73,7 +137,7 @@ def draw_boxes(img_path, good_forms, nb_ocr=0):
     with open(full_path_json, 'r') as json_file:
         data = json.load(json_file)
     
-    img, strings, df = draw_boxes_on_image(img_path, data, nb_ocr)
+    img, strings, df = cut_boxes_on_image(img_path, data, nb_ocr)
     
     return img, strings, df
 
